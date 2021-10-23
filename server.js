50a294c520d24e037f9593498ff2e96d0448b16a
Mariana <mariana.dantasweb7@gmail.com>
2021-10-23 20:33:39 -0300

    Install and test body-parser package

--- server.js
+++ server.js
@@ -5,14 +5,19 @@ const cors = require('cors')
 require('dotenv').config({
   path: path.join(__dirname, ".env")
 })
+const bodyParser = require('body-parser');
 
 app.use(cors())
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: false}))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
 
+app.post("/api/users", function(req, res){
+  res.json({"ok": req.body.username})
+})
 
 
 
