5f1595cad113e227c0318291fc2a6f38684dea9c
Mariana <mariana.dantasweb7@gmail.com>
2021-10-23 22:27:37 -0300

    Connect database and create user methods

--- server.js
+++ server.js
@@ -6,6 +6,35 @@ require('dotenv').config({
   path: path.join(__dirname, ".env")
 })
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+
+/* ************ Database configuration ************ */
+
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+
+const userSchema = new Schema({
+  username: String
+})
+
+const User = mongoose.model('User', userSchema);
+
+function findUser(id, done){
+  User.findById(id, function(err, user){
+    if(err) return console.error(err);
+    else done(null, user);
+  })
+}
+
+function saveUser(name, done){
+  const userToSave = new User({
+    username: name
+  });
+  userToSave.save(function(err, user){
+    if(err) return console.error(err);
+    else done(null, user);
+  })
+}
 
 app.use(cors())
 app.use(express.static('public'))
